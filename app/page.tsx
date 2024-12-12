859b7d64c5f25e35e02b09431f98c55899e5e46e
Gabriel Pablo González Fresard <legab@Gabriels-MacBook-Pro.local>
2024-12-12 18:15:56 -0300

    sss

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
 "use client"
 import { useEffect } from "react"
-import { getToken } from "firebase/messaging"
-import { messaging } from "@/lib/client/firebase.client"
+import {
+    getMessaging,
+    getToken,
+    isSupported,
+    Messaging,
+} from "firebase/messaging"
 import { firebaseConfig } from "@/lib/config/firebase.config"
+import { initializeApp } from "firebase/app"
 
 const Home = () => {
     useEffect(() => {
         const requestPermission = async () => {
+            const app = initializeApp(firebaseConfig)
+            let messaging: Messaging | null = null
+            await (async () => {
+                if (await isSupported()) {
+                    messaging = getMessaging(app)
+                } else {
+                    console.warn("FCM is not supported in this browser.")
+                }
+            })()
             const permission = await Notification.requestPermission()
             if (permission === "granted" && messaging) {
                 const token = await getToken(messaging, {
